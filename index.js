2d36243b4844a12e55dd5eeca706d58ad3fb6bf9
Dino Montez <dinomontez@192.168.1.24>
2026-04-18 22:08:27 -0300

    fix: dynamic port for render

--- index.js
+++ index.js
@@ -40,8 +40,10 @@ app.post('/api/render-video', upload.fields([
   })
 })
 
-app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000')
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log('Servidor rodando na porta ' + PORT)
 })
 app.get('/test', (req, res) => {
   res.send('ok')
